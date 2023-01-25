05c9ef13b68718174c8866506d04c4368da7b3c2
Sanket Suresh Thorat <112432267+Sanket-S-Thorat@users.noreply.github.com>
2023-01-25 18:52:45 +0530

    Add files via upload

--- 1.HA Multi-Az Env/main.tf
+++ 1.HA Multi-Az Env/main.tf
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 terraform {
   required_providers {
     aws = {
@@ -123,4 +124,131 @@ resource "aws_nat_gateway" "nat_gateway" {
   tags = {
     "Name" = "demo_nat_gateway"
   }
+=======
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.51.0"
+    }
+  }
+}
+
+#Configuring the provider
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.secret_access_key
+}
+
+#Getting all AZ available for the region
+data "aws_availability_zones" "available" {}
+data "aws_region" "current" {}
+
+#Creating the VPC
+resource "aws_vpc" "vpc" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name        = var.vpc_name
+    Environment = "demo_env"
+    Terraform   = "True"
+  }
+}
+
+#Creating the Private Subnets
+resource "aws_subnet" "private_subnets" {
+  for_each          = var.private_subnets
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, each.value)
+  availability_zone = tolist(data.aws_availability_zones.available.names)[each.value - 1]
+
+  tags = {
+    "Name"    = each.key
+    Terraform = "True"
+  }
+}
+
+#Deploy the Public Subnets
+resource "aws_subnet" "public_subnets" {
+  for_each                = var.public_subnets
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, each.value + 100)
+  availability_zone       = tolist(data.aws_availability_zones.available.names)[each.value - 1]
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name"    = each.key
+    Terraform = "True"
+  }
+}
+
+#Creating route tables for both types of subnets
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway.id 
+  }
+
+  tags = {
+    "Name"    = "demo_public_rtb"
+    Terraform = "True"
+  }
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+
+  tags = {
+    "Name"    = "demo_private_rtb"
+    Terraform = "True"
+  }
+}
+
+#Associating route tables
+resource "aws_route_table_association" "public" {
+  depends_on     = [aws_subnet.public_subnets]
+  route_table_id = aws_route_table.public_route_table.id
+  for_each       = aws_subnet.public_subnets
+  subnet_id      = each.value.id
+}
+
+resource "aws_route_table_association" "private" {
+  depends_on     = [aws_subnet.private_subnets]
+  route_table_id = aws_route_table.private_route_table.id
+  for_each       = aws_subnet.private_subnets
+  subnet_id      = each.value.id
+}
+
+#Creating Internet Gateway
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    "Name" = "demo_igw"
+  }
+}
+
+#Creating EIP for NAT Gateway
+resource "aws_eip" "nat_gateway_eip" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.internet_gateway]
+  tags = {
+    "Name" = "demo_igw_eip"
+  }
+}
+
+#Creating NAT Gateway
+resource "aws_nat_gateway" "nat_gateway" {
+  depends_on    = [aws_subnet.public_subnets]
+  allocation_id = aws_eip.nat_gateway_eip.id
+  subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
+  tags = {
+    "Name" = "demo_nat_gateway"
+  }
+>>>>>>> 063c30ed7f67b51b630ac79c8a63b57c3b944dbb
 }
--- 1.HA Multi-Az Env/variables.tf
+++ 1.HA Multi-Az Env/variables.tf
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 variable "aws_region" {
   type    = string
   default = "ap-south-1"
@@ -31,3 +32,38 @@ variable "private_subnets" {
 
 
 
+=======
+variable "aws_region" {
+  type    = string
+  default = "ap-south-1"
+}
+
+variable "vpc_name" {
+  type    = string
+  default = "demo_vpc"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  default = {
+    "public_subnet_1" = 1
+    "public_subnet_2" = 2
+    "public_subnet_3" = 3
+  }
+}
+
+variable "private_subnets" {
+  default = {
+    "private_subnet_1" = 1
+    "private_subnet_2" = 2
+    "private_subnet_3" = 3
+  }
+}
+
+
+
+>>>>>>> 063c30ed7f67b51b630ac79c8a63b57c3b944dbb
